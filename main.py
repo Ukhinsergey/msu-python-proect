0bbfb5b25ec727ededc06399451a6a248e00b3fd
Константин Кожемяков <konstantin.kozhemiakov@graphics.cs.msu.ru>
2021-05-16 19:56:27 +0300

    return to gunicorn

--- main.py
+++ main.py
@@ -7,25 +7,6 @@ import json
 app = Flask(__name__)
 bot = TwitchBot()
 
-@app.route('/post/', methods=['POST'])
-def post_something():
-    if request.method == 'GET':
-        return 'KEK'
-    else:
-        param = request.form.get('name')
-        print(param)
-        # You can add the test cases you made in the previous function, but in our case here you are just testing the POST functionality
-        if param:
-            return jsonify({
-                "Message": f"Welcome {param} to our awesome platform!!",
-                # Add this option to distinct the POST request
-                "METHOD" : "POST"
-            })
-        else:
-            return jsonify({
-                "ERROR": "no name found, please send a name."
-            })
-
 # A welcome message to test our server
 @app.route('/')
 def index():
