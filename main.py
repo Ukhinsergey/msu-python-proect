b36d6709999024240ec4e86a8987cf3feb6589ca
Константин Кожемяков <konstantin.kozhemiakov@graphics.cs.msu.ru>
2021-05-16 17:52:10 +0300

    classic flask app

--- main.py
+++ main.py
@@ -1,35 +1,51 @@
-from flask import Flask, request
-import json
-import os
-import sys
-
+# app.py
+from flask import Flask, request, jsonify
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Pay no attention to that man behind the curtain!"
+@app.route('/getmsg/', methods=['GET'])
+def respond():
+    # Retrieve the name from url parameter
+    name = request.args.get("name", None)
 
-@app.route("/kek", methods=["GET", "POST"])
-def test_handler():
-    if request.method == "GET":
-        return "my get"
-    elif request.method == "POST":
-        print('========')
-        # print(dir(request))
-        # print(request)
-        # print(type(request))
-        print(request.data)
-        print(request.form)
-        print('========')
-        return 'my post'
+    # For debugging
+    print(f"got name {name}")
 
+    response = {}
 
+    # Check if user sent a name at all
+    if not name:
+        response["ERROR"] = "no name found, please send a name."
+    # Check if the user entered a number not a name
+    elif str(name).isdigit():
+        response["ERROR"] = "name can't be numeric."
+    # Now the user entered a valid name
+    else:
+        response["MESSAGE"] = f"Welcome {name} to our awesome platform!!"
 
+    # Return the response in json format
+    return jsonify(response)
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    print(port)
-    print(port, file=sys.stderr)
-    app.run(
-        host="0.0.0.0", port=port, threaded=True
-    )
+@app.route('/post/', methods=['POST'])
+def post_something():
+    param = request.form.get('name')
+    print(param)
+    # You can add the test cases you made in the previous function, but in our case here you are just testing the POST functionality
+    if param:
+        return jsonify({
+            "Message": f"Welcome {param} to our awesome platform!!",
+            # Add this option to distinct the POST request
+            "METHOD" : "POST"
+        })
+    else:
+        return jsonify({
+            "ERROR": "no name found, please send a name."
+        })
+
+# A welcome message to test our server
+@app.route('/')
+def index():
+    return "<h1>Welcome to our server !!</h1>"
+
+if __name__ == '__main__':
+    # Threaded option to enable multiple instances for multiple user access support
+    app.run(threaded=True, port=5000)
