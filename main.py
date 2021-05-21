2793c336a2efeaca63b42e2a30b38e0ecc2461d7
sergei <ser191097@gmail.com>
2021-05-21 21:35:07 +0300

    test twitch token

--- main.py
+++ main.py
@@ -5,9 +5,11 @@ import os
 
 from flask import Flask, request
 from bot import TwitchBot
-
+from twitch import TwitchApi
 app = Flask(__name__)
 bot = TwitchBot()
+twitch_api = TwitchApi()
+
 
 @app.route('/')
 def index():
@@ -20,6 +22,12 @@ def test_author_response():
     if request.method == "POST":
         bot.bot.send_message(chat_id=234383022, text='I am working')
     return "ok"
+
+@app.route('/twitch-stat')
+def twitch_stat():
+    return twitch_api.twitch_app_token_json
+
+
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 443))
---
+++ twitch.py
@@ -0,0 +1,37 @@
+import requests
+import json
+import os
+
+
+
+
+class TwitchApi:
+    def __init__(self):
+        self.client_id = os.environ.get("CLIENT_ID", None)
+        self.client_secret = os.environ.get("CLIENT_SECRET", None) 
+        token_params = {
+            'client_id': clientid,
+            'client_secret': secret,
+            'grant_type': 'client_credentials',
+        }
+        app_token_request = requests.post('https://id.twitch.tv/oauth2/token', params=token_params)
+        self.twitch_app_token_json = app_token_request.json()
+
+        self.headers = {
+            'Client-ID': self.clientid,
+            'Content-type': 'application/json',
+            'Authorization': 'Bearer ' + self.twitch_app_token_json['access_token']
+        }
+
+        self.body = {
+            "type": "channel.follow",
+            "version": "1",
+            "condition": {
+                "broadcaster_user_id": "42674575"
+            },
+            "transport": {
+                "method": "webhook",
+                "callback": "https://heroku-testing-app228.herokuapp.com/",
+                "secret": self.client_secret 
+            }
+        }
