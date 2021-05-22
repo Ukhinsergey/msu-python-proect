f364ce9f1aa1d9e4462a6da1eb784caec49d704b
Константин Кожемяков <konstantin.kozhemiakov@graphics.cs.msu.ru>
2021-05-22 12:26:18 +0300

    added interactive keyboard

--- bot.py
+++ bot.py
@@ -19,8 +19,8 @@ import logging
 import json
 import os
 
-from telegram import Update
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+from telegram import Update, InlineKeyboardMarkup, InlineKeyboardButton
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext, CallbackQueryHandler
 from flask import request
 
 from database import Database
@@ -50,15 +50,17 @@ class TwitchBot(Updater):
 
     def _add_handlers(self) -> None:
         self.dispatcher.add_handler(CommandHandler("start", self.start))
+        self.dispatcher.add_handler(CommandHandler("help", self.help))
+        self.dispatcher.add_handler(CommandHandler("list", self.list_subs))
         
         self.dispatcher.add_handler(CommandHandler("subscribe", self.subscribe))
         self.dispatcher.add_handler(CommandHandler("sub", self.subscribe))
+        self.dispatcher.add_handler(CallbackQueryHandler(self.subscribe, pattern="^SUB_QUERY"))
 
         self.dispatcher.add_handler(CommandHandler("unsubscribe", self.unsubscribe))
         self.dispatcher.add_handler(CommandHandler("unsub", self.unsubscribe))
+        self.dispatcher.add_handler(CallbackQueryHandler(self.unsubscribe, pattern="^UNSUB_QUERY"))
 
-        self.dispatcher.add_handler(CommandHandler("help", self.help))
-        self.dispatcher.add_handler(CommandHandler("list", self.list_subs))
         self.dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
 
     def help(self, update: Update, _: CallbackContext) -> None:
@@ -120,9 +122,20 @@ class TwitchBot(Updater):
     def list_subs(self, update: Update, _: CallbackContext) -> None:
         try:
             result = self.database.get_subs_for_user(update.message.chat_id)
+            keyboard = [
+                InlineKeyboardButton(
+                    str(channel), callback_data=f"UNSUB_QUERY {channel}"
+                )
+                for channel in result
+            ]
+            keyboard = InlineKeyboardMarkup.from_column(keyboard)
             update.message.reply_text(
-                text="Ваши подписки:\n" + '\n'.join(map(str, result))
+                text="Ваши подписки:\n" +
+                "\n".join(map(str, result)) +
+                "Чтобы отписаться, нажмите на кнопку:\n",
+                reply_markup=keyboard
             )
+
         except Exception as exception:
             update.message.reply_text(
                 text=f"Возникла ошибка при извлечении списка подписок.\nПричина: {exception}"
