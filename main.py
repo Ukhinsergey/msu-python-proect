96e1cfec58dec8facc8b1fa874a6cc8a958d0fad
sergei <ser191097@gmail.com>
2021-05-24 23:41:31 +0300

    try to sub stream online dean1t

--- main.py
+++ main.py
@@ -35,6 +35,7 @@ def twitch_post():
         if "challenge" in data.keys():
             return data["challenge"], 200
         else:
+            bot.bot.send_message(chat_id=456145017, text=str(data))
             text_messsage = ""
             if data['subscription']['type'] == "channel.follow":
                 text_messsage = str(data['event']['user_name']) + " is following " + str(data['event']['broadcaster_user_name'])
@@ -42,7 +43,7 @@ def twitch_post():
                 text_messsage = str(data['event']['broadcaster_user_name']) + " is streaming now"
             bot.bot.send_message(chat_id=234383022, text=text_messsage)
             bot.bot.send_message(chat_id=456145017, text=text_messsage)
-            return "ok",200
+            return "ok", 200
     else:
         return 'get ' + str(twitch_api.info), 200
 
