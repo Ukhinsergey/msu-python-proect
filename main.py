5dc4e93da5d37da13f95cfa5647f3261a9fed1f2
Константин Кожемяков <konstantin.kozhemiakov@graphics.cs.msu.ru>
2021-05-25 01:54:17 +0300

    send notification to subbed users

--- main.py
+++ main.py
@@ -2,11 +2,13 @@
 TODO: Module docstring
 """
 import os
+import json
 
 from flask import Flask, request
+
 from bot import TwitchBot
 from twitch import TwitchApi
-import json
+
 app = Flask(__name__)
 bot = TwitchBot()
 twitch_api = TwitchApi(bot)
@@ -19,14 +21,20 @@ def index():
     """A welcome message to test our server"""
     return "<h1>Welcome to our server !!</h1>"
 
-@app.route("/test-webhook", methods=['GET', 'POST'])
-def test_author_response():
-    """Test webhook example"""
-    if request.method == "POST":
-        bot.bot.send_message(chat_id=234383022, text='I am working')
-        bot.bot.send_message(chat_id=456145017, text='I am working')
-    return "ok"
+def send_notification(twitch_id, display_name):
+    subscribed_users = bot.database.get_users_for_sub(twitch_id)
+    
+    # Check if display name has changed
+    saved_display_name = bot.database.get_channel_name(twitch_id)
+    if saved_display_name != display_name:
+        bot.database.delete_channel_name(twitch_id)
+        bot.database.put_channel_name(twitch_id, display_name)
 
+    for user_id in subscribed_users:
+        bot.bot.send_message(
+            chat_id=user_id, 
+            text=f"{display_name} is followed!"
+        )
 
 @app.route('/twitch_post', methods=['GET', 'POST'])
 def twitch_post():
@@ -41,6 +49,7 @@ def twitch_post():
             text_messsage = ""
             if data['subscription']['type'] == "channel.follow":
                 text_messsage = str(data['event']['user_name']) + " is following " + str(data['event']['broadcaster_user_name'])
+                send_notification(int(data['event']['broadcaster_user_id']), data['event']['broadcaster_user_name'])
             elif data['subscription']['type'] == "stream.online":
                 text_messsage = str(data['event']['broadcaster_user_name']) + " is streaming now"
             bot.bot.send_message(chat_id=234383022, text=text_messsage)
